e8f424d0b1fd65c4b09598d0fe0ed7709eacb5d6
Akshay Bhujbal <akshaysbhujbal90@gmail.com>
2025-09-09 20:13:38 +0530

    update
    .

--- cw1server/routes/tailorRoutes.js
+++ cw1server/routes/tailorRoutes.js
@@ -2,13 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const {gettailorinfo,gettailorTasks,updateTaskStatus,updateProfile,uploadProfileImage } = require("../controllers/tailorController");
-
+const { getTaskStatusCounts } = require("../controllers/tailorController");
 const { protect, authorize } = require("../middleware/authMiddleware");
 
 router.get("/gettailorinfo",protect,authorize("tailor"), gettailorinfo);
 // router.get("/:staffId/tasks", protect, authorize("tailor"), getTasksByStaff);
 router.get("/gettailortasks",protect,authorize("tailor"), gettailorTasks);
 router.put("/:taskId", protect,authorize("tailor"),updateTaskStatus);
+
+
+// GET /api/tasks/status-count
+router.get("/status-count", protect, authorize("tailor"), getTaskStatusCounts);
 
 
 module.exports = router;
